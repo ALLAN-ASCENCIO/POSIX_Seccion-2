c35b2d4ddabb12a3d02da47322063260f4e6ce4e
Allan <240300738@ucaribe.edu.mx>
2026-05-06 09:46:45 -0500

    Cambios en archivo tarea#977

--- Parcial_3/tarea#977.sh
+++ Parcial_3/tarea#977.sh
@@ -26,8 +26,28 @@ EASY_BackwardsListing() {
     pausar
 }
 
+#######################################################
+
 #Script 2
 INTERMEDIATE_ASCIItoInteger() {
+    limpiar
+    while true; do
+        mostrar_menu_ascii
+        read opcion_ascii
+        
+        case $opcion_ascii in
+            1)  ascii_to_integer ;;
+            2)  integer_to_ascii ;;
+            0)  break ;;
+            *)
+                echo "Opcion invalida. Presiona Enter para continuar..."
+                read
+                ;;
+        esac
+    done
+}
+    
+ascii_to_integer() {
     limpiar
     echo "INTERMEDIATE: ASCII to Integer"
     echo "-----------------------------"
@@ -39,10 +59,27 @@ INTERMEDIATE_ASCIItoInteger() {
     pausar
 }
 
-#Script 3
-DIFFICULT_SquareRoot() {
-
+integer_to_ascii() {
+    limpiar
+    echo "INTERMEDIATE: Integer to ASCII"
+    echo "-----------------------------"
+    echo "Introduce un número entero (0-127):"
+    read integer
+    if [[ $integer -ge 0 && $integer -le 127 ]]; then
+        ascii_char=$(printf "\\$(printf "%o" "$integer")")
+        echo "El carácter ASCII correspondiente a $integer es: '$ascii_char'"
+    else
+        echo "Número fuera de rango. Por favor, introduce un número entre 0 y 127."
+    fi
+    pausar
 }
+
+#######################################################
+
+#Script 3
+#DIFFICULT_SquareRoot() {
+
+#}
 
 
 #MENÚ PRINCIPAL
@@ -55,6 +92,17 @@ mostrar_menu() {
     echo "0. Salir"
     echo ""
     echo -n "Selecciona una opcion [0-3]: "
+}
+
+#MENÚ ASCII TO INTEGER
+mostrar_menu_ascii() {
+    limpiar
+    echo "Menu ASCII to Integer"
+    echo "1. ASCII to Integer"
+    echo "2. Integer to ASCII"
+    echo "0. Volver al menu principal"
+    echo ""
+    echo -n "Selecciona una opcion [0-2]: "
 }
 
 #BUCLE PRINCIPAL
